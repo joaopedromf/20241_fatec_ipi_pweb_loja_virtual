6b42d81ec9d88f39a9948cca3012726b2e0d2c1d
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2024-04-27 21:35:12 -0300

    feat(p1): criação dos components, models e rotas
    - Geração dos components para as páginas de cadastro, cesta, detalhe do produto, esqueci minha senha, login, pesquisa e vitrine.
    - Geração dos models da cesta, cliente, item e produto.
    - Configuração das rotas para as páginas dos components criados.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CestaComponent } from './cesta/cesta.component';
+import { DetalheComponent } from './detalhe/detalhe.component';
+import { EsqueciComponent } from './esqueci/esqueci.component';
+import { LoginComponent } from './login/login.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { VitrineComponent } from './vitrine/vitrine.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: VitrineComponent},
+    {path: "cadastro", component: CadastroComponent},
+    {path: "cesta", component: CestaComponent},
+    {path: "detalhe", component: DetalheComponent},
+    {path: "esqueci", component: EsqueciComponent},
+    {path: "login", component: LoginComponent},
+    {path: "pesquisa", component: PesquisaComponent},
+    {path: "vitrine", component: VitrineComponent},
+];
---
+++ src/app/vitrine/vitrine.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vitrine',
+  standalone: true,
+  imports: [],
+  templateUrl: './vitrine.component.html',
+  styleUrl: './vitrine.component.css'
+})
+export class VitrineComponent {
+
+}
