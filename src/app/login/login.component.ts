ec4e4e9e4e913b3e2d20750c346745aad7e40568
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2024-04-30 20:23:21 -0300

    feat(p1): criação do login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  public entrar(){
+    
+  }
 }
