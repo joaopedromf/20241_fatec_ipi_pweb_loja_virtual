ceb81df8b61caca3b242675f27ea711882b74f8d
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2024-04-30 23:12:35 -0300

    feat(p1): criação do cadastro e model cliente

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  
+  public obj: Cliente = new Cliente(); 
 
+  public gravar(){
+    console.log(JSON.stringify(this.obj));   
+    localStorage.setItem("cadastro", JSON.stringify(this.obj));
+  }
 }
--- src/app/model/cliente.ts
+++ src/app/model/cliente.ts
@@ -1,2 +1,13 @@
 export class Cliente {
+    public codigo: number = 0;
+    public nome: string = "";
+    public senha: string = "";
+    public email: string= "";
+    public telefone: string= "";
+    public cpf: string= "";
+    public cep: string= "";
+    public cidade: string= "";
+    public estado: string= "";
+    public endereco: string= "";
+    public complemento:string= "";
 }
