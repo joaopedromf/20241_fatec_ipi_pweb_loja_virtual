76ccaef86c8b3948cfa85500a54d6b33a73570ef
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2024-06-12 17:53:48 -0300

    style(pos-p1): adiciona carrossel nos detalhes
    - Carrossel de imagens adicionado nos detalhes do produto.
    - Pasta de imagens dos produtos foi organizada e novas imagens foram adicionadas.
    - Adicionado atributo modelo e tela para o produto.

--- src/app/model/produto.ts
+++ src/app/model/produto.ts
@@ -3,9 +3,11 @@ export class Produto {
     public codigo: number;
     public nome: string;
     public marca: string;
+    public modelo: string;
     public cor: string;
     public memoriaInterna: number;
     public memoriaRam: number;
+    public tela: number;
     public descritivo: string;
     public valor: number;
     public valorPromo: number;
@@ -16,9 +18,11 @@ export class Produto {
         this.codigo = 0;
         this.nome = "";
         this.marca = "";
+        this.modelo = "";
         this.cor = "";
         this.memoriaInterna = 0;
         this.memoriaRam = 0;
+        this.tela = 0.0;
         this.descritivo = "";
         this.valor = 0;
         this.valorPromo = 0;
