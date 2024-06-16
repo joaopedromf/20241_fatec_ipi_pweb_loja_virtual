ab242737abf32cf5f7667d78571d6682f58da0d3
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2024-06-15 23:54:09 -0300

    feat(p2): implementa Produto completo
    - Cria entidade, repositório e controlador para o Produto.

---
+++ backend/src/main/java/com/fatec/loja/controller/ProdutoController.java
@@ -0,0 +1,81 @@
+package com.fatec.loja.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.loja.entity.ProdutoEntity;
+import com.fatec.loja.repository.ProdutoRepository;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class ProdutoController {
+    
+    @Autowired
+    ProdutoRepository repository;
+
+    @GetMapping("/api/produtos")
+    public ResponseEntity<List<ProdutoEntity>> listar(){
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/api/produto/{codigo}")
+    public ResponseEntity<ProdutoEntity> carregar(@PathVariable int codigo){
+        Optional<ProdutoEntity> obj = repository.findById(codigo);
+        if(obj.isPresent()){
+            return ResponseEntity.ok(obj.get());
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/api/produto")
+    public ResponseEntity<ProdutoEntity> gravar(@RequestBody ProdutoEntity produto){
+        produto.setNome();
+        repository.save(produto);
+        return ResponseEntity.status(201).body(produto);
+    }
+
+    @PutMapping("/api/produto")
+    public ResponseEntity<ProdutoEntity> atualizar(@RequestBody ProdutoEntity produto){
+        Optional<ProdutoEntity> obj = repository.findById(produto.getCodigo());
+        if(obj.isPresent()){
+            produto.setNome();
+            repository.save(produto);
+            return ResponseEntity.ok(produto);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/api/produto/{codigo}")
+    public ResponseEntity<Void> remover(@PathVariable int codigo){
+        Optional<ProdutoEntity> obj = repository.findById(codigo);
+        if(obj.isPresent()){
+            repository.deleteById(codigo);;
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/api/produto/busca/{pesquisa}")
+    public ResponseEntity<List<ProdutoEntity>> buscar(@PathVariable String pesquisa){
+        String aux = '%'+ pesquisa +'%';
+        List<ProdutoEntity> lista = repository.buscar(aux);
+        return ResponseEntity.ok(lista);
+    }
+}
---
+++ backend/src/main/java/com/fatec/loja/entity/ProdutoEntity.java
@@ -0,0 +1,153 @@
+package com.fatec.loja.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class ProdutoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int codigo;
+    private String nome;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int memoriaInterna;
+    private int memoriaRam;
+    private double tela;
+    @Column(length = 5000)
+    private String descritivo;
+    private double valor;
+    private double valorPromo;
+    private boolean destaque;
+    private int estoque;
+    
+    public ProdutoEntity() {
+    }
+
+    public ProdutoEntity(int codigo, String marca, String modelo, String cor, int memoriaInterna, int memoriaRam, double tela, String descritivo, double valor, double valorPromo, boolean destaque, int estoque){
+        this.codigo = codigo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.memoriaInterna = memoriaInterna;
+        this.memoriaRam = memoriaRam;
+        this.tela = tela;
+        this.descritivo = descritivo;
+        this.valor = valor;
+        this.valorPromo = valorPromo;
+        this.destaque = destaque;
+        this.estoque = estoque;
+        setNome();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome() {
+        nome = marca + " " + modelo + " " +  memoriaInterna + "GB " + cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getMemoriaInterna() {
+        return memoriaInterna;
+    }
+
+    public void setMemoriaInterna(int memoriaInterna) {
+        this.memoriaInterna = memoriaInterna;
+    }
+
+    public int getMemoriaRam() {
+        return memoriaRam;
+    }
+
+    public void setMemoriaRam(int memoriaRam) {
+        this.memoriaRam = memoriaRam;
+    }
+
+    public double getTela() {
+        return tela;
+    }
+
+    public void setTela(double tela) {
+        this.tela = tela;
+    }
+
+    public String getDescritivo() {
+        return descritivo;
+    }
+
+    public void setDescritivo(String descritivo) {
+        this.descritivo = descritivo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValorPromo() {
+        return valorPromo;
+    }
+
+    public void setValorPromo(double valorPromo) {
+        this.valorPromo = valorPromo;
+    }
+
+    public boolean isDestaque() {
+        return destaque;
+    }
+
+    public void setDestaque(boolean destaque) {
+        this.destaque = destaque;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
